5874cf9f10e308a6d200b05ec67cc3405e208714
root <root@fen-vz-puppet.fen.aptivate.org>
2011-06-08 12:38:23 +0000

    Add support for Debian and Ubuntu.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,22 +1,43 @@
+# vim: set expandtab ts=4 sw=4:
+
 # Class: nfsclient
 #
 # Setup an NFS client
-#
+
 class nfsclient { 
+    case $lsbdistid {
+        Debian, Ubuntu: { 
+            package { "nfs-common":
+                # ensure => installed,
+                ;
+            } 
+            service {
+                "nfs-common":
+                    pattern => "rpc.statd";
+            }
+        }
+        CentOS: {
+            package { "nfs-utils":
+                ;
+            }
+            service {
+                netfs:;
+                nfslock:
+                    pattern => "rpc.statd";
+            }
+        }
+    }
 
     package {
-        "nfs-utils":;
         "portmap":;
     } # package
 
     Service { enable => true }
-
+    
     service { 
-        "netfs":;
-        "nfslock":
-            ensure  => running, 
-            pattern => "rpc.statd";
         "portmap":
              ensure => running;
     } # service
+
+
 } # class nfsclient
